82d506cd5ffaabd256be164073e9da54762dfb3c
nojaf <florian.verdonck@outlook.com>
2026-04-09 16:30:01 +0200

    Offline mode for VAT page

---
+++ vat-sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'vat-v1';
+const ASSETS = [
+  '/vat.html',
+  '/vat.css',
+  '/images/logo.svg',
+  '/apple-touch-icon.png',
+  '/favicon-32x32.png',
+  '/favicon-16x16.png',
+  '/favicon.ico',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
